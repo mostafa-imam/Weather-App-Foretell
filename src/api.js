f799e166ee0e107ac7f3c8c4df7981042a9d9011
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2025-12-14 20:27:01 +0200

    fix: Fix giphy api url display issue

--- src/api.js
+++ src/api.js
@@ -3,7 +3,9 @@ import { resetWeatherInformation, resetGif } from "./helpers.js";
 
 async function hitVisualCrossing(location) {
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=YYD6UJSJRZY2ADQDCNSEUS8RZ`);
+        const response = await fetch(
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=YYD6UJSJRZY2ADQDCNSEUS8RZ`,
+        );
         if (response.ok) {
             return await response.json();
         } else if (response.status > 299) {
@@ -17,15 +19,18 @@ async function hitVisualCrossing(location) {
 
 async function hitGif(condition, img) {
     try {
-        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=//M1Vf9bS9gSWi3Cq2yezsEVOarlkrWnyd&s=${condition}&weirdness=0`);
+        const response = await fetch(
+            `https://api.giphy.com/v1/gifs/translate?api_key=M1Vf9bS9gSWi3Cq2yezsEVOarlkrWnyd&s=${condition}&weirdness=0`,
+        );
         if (response.ok) {
-            const gif = await response.json()
-            img.src = gif.data.images.original.url;
+            const gif = await response.json();
+
+            img.style.backgroundImage = `url(${gif.data.images.original.url})`;
         } else if (response.status > 299) {
-            throw new Error("there is some error")
+            throw new Error("there is some error");
         }
     } catch (error) {
         console.log(error);
-        resetGif();
+        resetGif(img);
     }
 }
